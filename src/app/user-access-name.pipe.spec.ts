10bdfcac72b547d2c2283eca9c32b95adbc25e1f
Lucas Dal Pozzo <Lucasdpmatos@gmail.com>
2024-01-19 15:01:30 -0300

    removed all .spec files

--- src/app/user-access-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserAccessNamePipe } from './user-access-name.pipe';
-
-describe('UserAccessNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserAccessNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
